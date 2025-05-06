0d6ae4b44c89ffb9a2aeb79815386312b3b9a600
LongChengCang <canglongcheng87@gmail.com>
2025-05-06 23:09:34 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -4,12 +4,12 @@ const CONFIG = {
 
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2025-05-01', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://www.piscesman.top' },
-    { title: '访问文档中心获取更多帮助', url: 'https://www.piscesman.top' }
+    { title: '欢迎来到我的博客', url: 'https://piscesman.top' },
+    { title: '访问文档中心获取更多帮助', url: 'https://piscesman.top' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -21,16 +21,16 @@ const CONFIG = {
   HEO_HERO_TITLE_1: '分享策略',
   HEO_HERO_TITLE_2: '与思维认知',
   HEO_HERO_TITLE_3: 'piscesman.top',
-  HEO_HERO_TITLE_4: '新版上线',
+  HEO_HERO_TITLE_4: '建设中……',
   HEO_HERO_TITLE_5: '双鱼说交易建设中……',
   HEO_HERO_TITLE_LINK: 'https://www.piscesman.top',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '技术分享', url: '/tag/技术' },
+  HEO_HERO_CATEGORY_2: { title: '推荐文章', url: '/tag/推荐' },
+  HEO_HERO_CATEGORY_3: { title: '知行合一', url: '/tag/思考' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
